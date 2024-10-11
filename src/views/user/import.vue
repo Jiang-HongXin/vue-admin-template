ee33d04e010bd65f595903de9b441512af8cc7f5
jianghongxin <jianghongxin@lizhi.fm>
2024-10-11 11:55:12 +0800

    feat:教师导入优化

--- src/views/user/import.vue
+++ src/views/user/import.vue
@@ -10,11 +10,13 @@
         <el-upload
           class="upload-demo"
           action=""
+          :before-upload="beforeUpload"
           :http-request="uploadFiles"
           :on-change="handleChange"
           :on-remove="handleRemove"
           :file-list="fileList"
           :limit="1"
+          :show-file-list="false"
         >
           <el-button size="small" type="primary">点击上传</el-button>
         </el-upload>
@@ -26,7 +28,7 @@
 
 <script>
 
-import {Message} from "element-ui";
+import {Message, MessageBox} from "element-ui";
 import {exportTpl, importData} from "@/api/admin";
 
 
@@ -59,13 +61,30 @@ export default {
     handleRemove(item) {
       this.fileIndexMap.delete(item.name)
     },
+    beforeUpload(file) {
+      let types = ['xls', 'xlsx'];
+      const fileType = file.name.substring(file.name.lastIndexOf(".") + 1);
+      const isExcel = types.includes(fileType);
+      if (!isExcel) {
+        this.$message.error('文件只能是 Excel！仅接受 XLSX、XLS 格式!');
+        return Promise.reject(false);
+      }
+      return true;
+    },
     uploadFiles(item) {
       let formData = new FormData()
       formData.append('file', item.file)
       importData(formData).then(response => {
-        if (response.code === 0) {
-          this.fileIndexMap.set(item.file.name, response.data)
-        }
+        this.fileIndexMap.set(item.file.name, response.data)
+        this.$message({
+          message: '操作成功! 请前往人员列表进行查看！',
+          type: 'success'
+        })
+      }).catch(error => {
+        this.$message({
+          message: '操作成功! 请前往人员列表进行查看！',
+          type: 'error'
+        })
       })
     }
   }
